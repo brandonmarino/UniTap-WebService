7fb99a1266add82dac2988a36b090e653f44a8fa
osamabuhamad <roma4ever-10@hotmail.com>
2016-03-10 07:19:42 +0000

    changing the About us page

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -16,9 +16,6 @@ class PageControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  # test "should get signup" do
-  #   get :signup
-  #   assert_response :success
-  # end
+
 
 end
