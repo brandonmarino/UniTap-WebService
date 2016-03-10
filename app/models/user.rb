1780324e4f53a9cf96ff0325b162d0e15d876064
osamabuhamad <roma4ever-10@hotmail.com>
2016-03-10 05:00:10 +0000

    adding devise gem to add authintications and changing the layout

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :userid
+  has_one :name 
+  has_many :cards
+  
+end
